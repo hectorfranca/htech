1f4bd7f2cc658b160bba50be40dc47eb609dd1f2
hectorfranca <hectorjuliomartinsdefranca@hotmail.com>
2024-03-06 18:22:07 -0300

    update project

--- scripts/menu.js
+++ scripts/menu.js
@@ -5,8 +5,46 @@ servicesSection = document.getElementsByClassName('services')[0]
 pricesSection = document.getElementsByClassName('prices')[0]
 aboutSection = document.getElementsByClassName('about')[0]
 topButton = document.getElementsByClassName('top-link')[0]
+menuMobile = document.getElementsByClassName('menu-mobile')[0]
+menu = document.getElementsByClassName('menu')[0]
 
-serviceItem.addEventListener('click', () => window.scrollTo(0, servicesSection.offsetTop - 80))
-priceItem.addEventListener('click', () => window.scrollTo(0, pricesSection.offsetTop - 80))
-aboutItem.addEventListener('click', () => window.scrollTo(0, aboutSection.offsetTop - 80))
+function preventDefault(e) {
+    e.preventDefault();
+}
+
+function disableScroll() {
+    window.addEventListener('touchmove', preventDefault, {passive: false});
+}
+function enableScroll() {
+    window.removeEventListener('touchmove', preventDefault, {passive: false});
+}
+
+// Menu item
+serviceItem.addEventListener('click', () => {
+    window.scrollTo(0, servicesSection.offsetTop - 80)
+    menu.style.display = 'none';
+    enableScroll()
+})
+priceItem.addEventListener('click', () => {
+    window.scrollTo(0, pricesSection.offsetTop - 80)
+    menu.style.display = 'none';
+    enableScroll()
+})
+aboutItem.addEventListener('click', () => {
+    window.scrollTo(0, aboutSection.offsetTop - 80)
+    menu.style.display = 'none';
+    enableScroll()
+})
+
 topButton.addEventListener('click', () => window.scrollTo(0, 0))
+
+// Mobile button
+menuMobile.addEventListener('click', () => {
+    if (menu.style.display === 'flex') {
+        menu.style.display = 'none';
+        enableScroll()
+    } else {
+        menu.style.display = 'flex';
+        disableScroll()
+    }
+})
